7faf048e5fb0a0f0405719c6bd68aaaeeeb445f6
MalekBenJemia <malekbenjemia@gmail.com>
2021-02-19 17:51:10 -0500

    end

--- routes/apiRoutes/voteRoutes.js
+++ routes/apiRoutes/voteRoutes.js
@@ -3,6 +3,30 @@ const router = express.Router();
 const db = require('../../db/database');
 const inputCheck = require('../../utils/inputCheck');
 
+// Get the total votes for all the candidates
+router.get('/votes', (req, res) => {
+  const sql =  `SELECT candidates.*, parties.name AS party_name, 
+                COUNT(candidate_id) 
+                AS count FROM votes 
+                LEFT JOIN candidates ON votes.candidate_id = candidates.id 
+                LEFT JOIN parties ON candidates.party_id = parties.id 
+                GROUP BY candidate_id 
+                ORDER BY count DESC`;
+  const params = [];
+  db.all(sql, params, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+
+    res.json({
+      message: 'success',
+      data: rows
+    });
+  });
+});
+
+// Create a vote record
 router.post('/vote', ({body}, res) => {
   // Data validation 
   const errors = inputCheck(body, 'voter_id', 'candidate_id');
@@ -10,12 +34,10 @@ router.post('/vote', ({body}, res) => {
     res.status(400).json({ error: errors });
     return;
   }
-
-  // Prepare statement
-  const sql = `INSERT INTO votes (voter_id, candidate_id) VALUES (?, ?)`;
+  
+  const sql = `INSERT INTO votes (voter_id, candidate_id) VALUES (?,?)`;
   const params = [body.voter_id, body.candidate_id];
-
-  // Execute
+  // use ES5 function, not arrow to use this 
   db.run(sql, params, function(err, result) {
     if (err) {
       res.status(400).json({ error: err.message });
@@ -30,31 +52,4 @@ router.post('/vote', ({body}, res) => {
   });
 });
 
-
-
-router.get('/vote', (req, res) => {
-  
-  // Prepare statement
-  const sql = `SELECT candidates.*, parties.name AS party_name, COUNT(candidate_id) AS count
-                FROM votes
-                LEFT JOIN candidates ON votes.candidate_id = candidates.id
-                LEFT JOIN parties ON candidates.party_id = parties.id
-                GROUP BY candidate_id ORDER BY count DESC;`;
-  const params = [];
-  // Execute
-  db.run(sql, params, function(err, result) {
-    if (err) {
-      res.status(500).json({ error: err.message });
-      return;
-    }
-
-    res.json({
-      message: 'success',
-      data: rows,
-    });
-  });
-});
-
-
-
-  module.exports = router;
+module.exports = router;
